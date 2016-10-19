4b0ff9e2722606a103375ad5086f5014eb837a14
Andrew Angell <andrew@angelleye.com>
2016-10-19 02:57:27 -0500

    Adjusts PHP synatax for loading variables, ref #10

--- addNewPlayer.php
+++ addNewPlayer.php
@@ -1,14 +1,14 @@
 <?php
 
 include './includes/dbconfig.php';
-$firstname = $_POST['firstname'];
-$lastname  = $_POST['lastname'];
-$mname     = $_POST['mname'];
-$bdate     = $_POST['bdate'];
-$sex       = $_POST['emodel'];
-$rating    = $_POST['rating'];
-$email     = $_POST['email'];
-$usatt     = $_POST['usatt'];
+$firstname = !empty($_POST['firstname']) ? $_POST['firstname'] : '';
+$lastname  = !empty($_POST['lastname']) ? $_POST['lastname'] : '';
+$mname     = !empty($_POST['mname']) ? $_POST['mname'] : '';
+$bdate     = !empty($_POST['bdate']) ? $_POST['bdate'] : '';
+$sex       = !empty($_POST['emodel']) ? $_POST['emodel'] : '';
+$rating    = !empty($_POST['rating']) ? $_POST['rating'] : '';
+$email     = !empty($_POST['email']) ? $_POST['email'] : '';
+$usatt     = !empty($_POST['usatt']) ? $_POST['usatt'] : '';
 
 $query = mysqli_query($con, 'INSERT INTO users (member_id, fname,lname,mname,sex,rating,expiration,last_played,email) 
                VALUES ("' . $usatt . '","' . $firstname . '","' . $lastname . '","'.$mname.'","'.$sex.'","'.$rating.'","'.$expiration.'","'.$last_played.'","'.$email.'")');
